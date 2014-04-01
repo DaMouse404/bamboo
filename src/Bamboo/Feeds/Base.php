208d6376ff66974e66b0e00103c609c4efa42c60
Craig Taub <craig.taub@bbc.co.uk>
2014-04-01 14:38:39 +0100

    squash

--- src/Bamboo/Feeds/Base.php
+++ src/Bamboo/Feeds/Base.php
@@ -31,16 +31,16 @@ class Base
         $encoding = "UTF-8";
 
         return mb_strtoupper(
-                    mb_substr($string, 0, 1, $encoding), 
-                    $encoding
-                ) . 
-                mb_strtolower(
-                    mb_substr(
-                        $string, 1, mb_strlen($string), 
-                        $encoding
-                    ),
-                    $encoding
-                );
+            mb_substr($string, 0, 1, $encoding), 
+            $encoding
+        ) . 
+        mb_strtolower(
+            mb_substr(
+                $string, 1, mb_strlen($string), 
+                $encoding
+            ),
+            $encoding
+        );
     }
 
 }
