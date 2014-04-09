ea0831828d2ae4ee79ce4014e19939cdb5c2f7e8
Craig Taub <craig.taub@bbc.co.uk>
2014-04-09 11:57:21 +0100

    EPisode and Programme feeds

---
+++ src/Bamboo/Feeds/Episodes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bamboo\Feeds;
+
+use Bamboo\Models\Episode;
+
+class Episodes extends Base
+{
+
+    protected $_feed = 'episodes/{pids}';
+    protected $_response;
+
+    public function __construct($params, $pids) {
+        $this->_setPids($pids);
+        parent::__construct($params);
+    }
+
+    private function _setPids($pids) {
+        $pids = join($pids, ",");
+        $this->_feed = str_replace("{pids}", $pids, $this->_feed); 
+    }
+
+    /*
+     * Return array of Channel models
+     */
+    public function getElements() {
+        $episodes = array();
+        foreach ($this->_response->episodes as $episode) {
+            $episodes[] = new Episode($episode);
+        }
+
+        return $episodes;
+    }
+
+}
---
+++ src/Bamboo/Feeds/Programmes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bamboo\Feeds;
+
+use Bamboo\Models\Programme\Large;
+
+class Programmes extends Base
+{
+
+    protected $_feed = 'programmes/{pids}';
+    protected $_response;
+
+    public function __construct($params, $pids) {
+        $this->_setPids($pids);
+        parent::__construct($params);
+    }
+
+    private function _setPids($pids) {
+        $pids = join($pids, ",");
+        $this->_feed = str_replace("{pids}", $pids, $this->_feed); 
+    }
+
+    /*
+     * Return array of Channel models
+     */
+    public function getElements() {
+        $programmes = array();
+        foreach ($this->_response->programmes as $programme) {
+            $programmes[] = new Large($programme);
+        }
+
+        return $programmes;
+    }
+
+}
