6764691939a771d19c3bb1339f81833fa5cff63e
Craig Taub <craig.taub@bbc.co.uk>
2014-04-09 11:29:04 +0100

    Programme Episode for GROUPS

---
+++ src/Bamboo/Feeds/Programmes/Episodes.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bamboo\Feeds\Programmes;
+
+use Bamboo\Feeds\Base;
+use Bamboo\Models\Episode;
+use Bamboo\Models\Group\Large;
+
+class Episodes extends Base
+{
+
+    protected $_feed = 'programmes/{id}/episodes';
+    protected $_response;
+
+    public function __construct($params, $id) {
+        $this->_setId($id);
+        parent::__construct($params);
+    }
+
+    private function _setId($id) {
+        $this->_feed = str_replace("{id}", $id, $this->_feed); 
+    }
+
+    public function getEpisodes() {
+        $elements = array();
+
+        foreach ($this->_response->programme_episodes->elements as $item) {
+            $className = $this->_className($item->type);
+            $elements[] = new $className($item);
+        }
+        
+        return $elements;
+    }
+
+
+}
