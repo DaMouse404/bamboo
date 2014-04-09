16d97555e88aeee409692bf9d9d3a7621bdaea3e
Craig Taub <craig.taub@bbc.co.uk>
2014-04-09 16:37:40 +0100

    Episode recommendations feed object

---
+++ src/Bamboo/Feeds/Episodes/Recommendations.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bamboo\Feeds\Episodes;
+
+use Bamboo\Feeds\Base;
+use Bamboo\Models\Episode;
+use Bamboo\Models\Group\Large;
+
+class Recommendations extends Base
+{
+
+    protected $_feed = 'episodes/{pid}/recommendations';
+    protected $_response;
+
+    public function __construct($params, $id) {
+        $this->_setPid($id);
+        parent::__construct($params);
+    }
+
+    private function _setPid($pid) {
+        $this->_feed = str_replace("{pid}", $pid, $this->_feed); 
+    }
+
+    public function getElements() {
+        $elements = array();
+
+        foreach ($this->_response->episode_recommendations->elements as $item) {
+            $className = $this->_className($item->type);
+            $elements[] = new $className($item);
+        }
+        
+        return $elements;
+    }
+
+
+}
