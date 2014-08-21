9af95105cd3e074f4b49978da112d13ae8055d24
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2014-08-21 17:07:16 +0100

    Created Feed Model + Tests for Broadcasts feed

---
+++ src/Bamboo/Feeds/Broadcasts.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bamboo\Feeds;
+
+use Bamboo\Models\Broadcast;
+
+class Broadcasts extends Base
+{
+
+    protected $_feed = '/channels/{channel_id}/broadcasts';
+    protected $_response;
+
+    public function __construct($params, $channel) {
+        $this->_setChannel($channel);
+        parent::__construct($params);
+    }
+
+    private function _setChannel($channel) {
+        $this->_feed = str_replace("{channel}", $channel, $this->_feed); 
+    }
+
+    public function getBroadcasts() {
+        $broadcasts = array();
+        foreach ($this->_response->broadcasts->elements as $broadcast) {
+            $broadcasts[] = new Broadcast($broadcast);
+        }
+        return $broadcasts;
+    }
+}
