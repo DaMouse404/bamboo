1c4bbd4ef92d6f3fc12bb3a4d32f913e90b20aa5
Christopher Sidebottom <Christopher.Sidebottom@bbc.co.uk>
2014-09-01 17:45:37 +0100

    Add new Availability methods for stats calls and improve Version coverage

--- tests/Bamboo/Tests/Models/VersionTest.php
+++ tests/Bamboo/Tests/Models/VersionTest.php
@@ -7,6 +7,78 @@ use Bamboo\Models\Version;
 
 class VersionTest extends BambooTestCase
 {
+    public function testGetTotalDaysAvailabile() {
+        $version = $this->_createVersion(array('availability' => (object) array(
+                'start' => '2014-08-30T12:00:00Z',
+                'end' => '2014-09-06T12:00:00Z'
+            )
+        ));
+
+        $this->assertEquals(7, $version->getTotalDaysAvailable());
+    }
+
+    public function testNoAvailability() {
+        $version = $this->_createVersion(array());
+        $this->assertEmpty($version->getTotalDaysAvailable());
+    }
+
+    public function testGetRemainingDaysAvailable() {
+        $version = $this->_createVersion(array('availability' => (object) array(
+            'end' => date('c', strtotime('+5 days'))
+        )));
+
+        $this->assertEquals(5, $version->getRemainingDaysAvailable());
+    }
+
+    public function testGetAvailabilityDay() {
+        $version = $this->_createVersion(array('availability' => (object) array(
+            'start' => date('c', strtotime('-3 days')),
+            'end' => date('c', strtotime('+5 days'))
+        )));
+
+        // This is in the 4th period
+        $this->assertEquals(4, $version->getAvailabilityDay());
+    }
+
+    public function testNoDaysRemainingAvailabile() {
+        $version = $this->_createVersion(array());
+        $this->assertEmpty($version->getRemainingDaysAvailable());
+    }
+
+    public function testGetRemainingAvailability() {
+        $version = $this->_createVersion(array('availability' => (object) array(
+            'remaining' => (object) array(
+                'text' => 'Available until the end of time',
+                'value' => 'P4DT21H24M17S'
+            )
+        )));
+
+        $this->assertEquals('Available until the end of time', $version->getRemainingAvailability());
+    }
+
+    public function testNoRemainingAvailability() {
+        $version = $this->_createVersion(array());
+
+        $this->assertEmpty($version->getRemainingAvailability());
+    }
+
+    public function testGetAvailability() {
+        $version = $this->_createVersion(array('availability' => (object) array(
+                'start' => '2014-08-30T12:00:00Z',
+                'end' => '2014-09-06T12:00:00Z'
+            )
+        ));
+
+        $this->assertEquals('2014-08-30T12:00:00Z', $version->getAvailability('start'));
+        $this->assertEquals('2014-09-06T12:00:00Z', $version->getAvailability());
+        $this->assertEmpty($version->getAvailability('cake'));
+    }
+
+    public function testNoDuration() {
+        $version = $this->_createVersion(array());
+        $this->assertEmpty($version->getDuration());
+    }
+
     public function testSlugForOriginalVersion() {
         $version = $this->_createVersion(array('kind' => 'original'));
         $this->assertEquals('', $version->getSlug());
@@ -27,6 +99,19 @@ class VersionTest extends BambooTestCase
         $this->assertEquals('', $version->getSlug());
     }
 
+    public function testIncorrectKindRetrievingOnwardJourney() {
+        $version = $this->_createVersion(
+            array(
+                'events' => array(
+                    (object) array(
+                        'kind' => 'cake'
+                    )
+                )
+            )
+        );
+        $this->assertEmpty($version->getOnwardJourneyTime());
+    }
+
     public function testRetrievingOnwardJourneyTime() {
         $timeOffset = '30';
         $version = $this->_createVersion(
@@ -42,6 +127,16 @@ class VersionTest extends BambooTestCase
         $this->assertEquals($timeOffset, $version->getOnwardJourneyTime());
     }
 
+    public function testNoOnwardJourney() {
+        $version = $this->_createVersion(array());
+        $this->assertEmpty($version->getOnwardJourneyTime());
+    }
+
+    public function testGetGuidance() {
+        $version = $this->_createVersion(array('guidance'=>'Cake'));
+        $this->assertEquals('Cake', $version->getGuidanceObj());
+    }
+
     public function testGuidanceData() {
         $version = $this->_createVersion(
             (object) array('guidance' => (object) array(
@@ -55,6 +150,20 @@ class VersionTest extends BambooTestCase
         $this->assertEquals('small text', $version->getSmallGuidance());
         $this->assertEquals('medium text', $version->getMediumGuidance());
         $this->assertEquals('large text', $version->getLargeGuidance());
+    }
+
+    public function testGuidanceId() {
+        $version = $this->_createVersion(
+            (object) array('guidance' => (object) array(
+                'id' => 'cake'
+            ))
+        );
+        $this->assertEquals('cake', $version->getGuidanceID());
+    }
+
+    public function testNoGuidanceid() {
+        $version = $this->_createVersion(array());
+        $this->assertEmpty($version->getGuidanceID());
     }
 
     public function testEmptyGuidanceData() {
@@ -98,6 +207,35 @@ class VersionTest extends BambooTestCase
         $this->assertEquals(4800, $this->_getSecsForDuration('80 mins', 'PT1H20M'));
         $this->assertEquals(600, $this->_getSecsForDuration('10 mins', 'PT10M'));
         $this->assertEquals(630, $this->_getSecsForDuration('11 mins', 'PT10M30S'));
+    }
+
+    public function testGetRRC() {
+        $ver = $this->_createVersion(array('rrc'=>'Cake'));
+        $this->assertEquals('Cake', $ver->getRRC());
+    }
+
+    public function testGetRRCAttrs() {
+        $version = $this->_createVersion(
+            (object) array('rrc' => (object) array(
+                'description' => (object) array(
+                    'small' => 'small text',
+                    'large' => 'large text'
+                ),
+                'url' => 'cake'
+            ))
+        );
+
+        $this->assertEquals('small text', $version->getRRCShort());
+        $this->assertEquals('large text', $version->getRRCLong());
+        $this->assertEquals('cake', $version->getRRCUrl());
+    }
+
+    public function testNoRRCAttrs() {
+        $version = $this->_createVersion(array());
+
+        $this->assertEmpty($version->getRRCShort());
+        $this->assertEmpty($version->getRRCLong());
+        $this->assertEmpty($version->getRRCUrl());
     }
 
     private function _getSecsForDuration($text, $value) {
