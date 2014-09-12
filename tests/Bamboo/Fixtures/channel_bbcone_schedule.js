b58ef5caafc45a2fd646870260d26cbcf6c4548b
Dineshraj Goomany <dineshraj.goomany@bbc.co.uk>
2014-09-12 13:57:41 +0100

    Added fixture fix to stop PR failing

--- tests/Bamboo/Fixtures/channel_bbcone_schedule.js
+++ tests/Bamboo/Fixtures/channel_bbcone_schedule.js
@@ -1,5 +1,21 @@
 module.exports = function (creator, fixtureName) {
-    return creator.createFixture('channels/bbc_one_london/schedule/2014-08-10').then(function (fixture) {
+    var today = new Date(),
+        day = today.getDate(),
+        month = today.getMonth() + 1,
+        year = today.getFullYear(),
+        timestamp;
+
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    if (month < 10) {
+        month = '0' + month;
+    }
+
+    timestamp = [year, month, day].join('-');
+
+    return creator.createFixture('channels/bbc_one_london/schedule/' + timestamp).then(function (fixture) {
         fixture.save(fixtureName);
     });
 };
