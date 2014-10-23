8abaaec830dc4765c2fa4609f8015e64d3dff0a0
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2014-10-23 13:45:33 +0100

    Add promotion

--- tests/Bamboo/Fixtures/tv_highlights.js
+++ tests/Bamboo/Fixtures/tv_highlights.js
@@ -1,5 +1,11 @@
 module.exports = function (creator, fixtureName) {
     return creator.createFixture('tv/highlights').then(function (fixture) {
+        fixture.addEpisode(0).set({
+            type: "promotion",
+            labels: {
+                "promotion": "promotion"
+            }
+        });
         fixture.save(fixtureName);
     });
 };
