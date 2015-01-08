e6c37a91fcb840d6e93f9287ebee3c4058ba67ee
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-01-08 09:48:55 +0000

    Also test the header key

--- tests/Bamboo/Tests/CacheStorageTest.php
+++ tests/Bamboo/Tests/CacheStorageTest.php
@@ -9,22 +9,45 @@ use \Guzzle\Http\EntityBody;
 class CacheStorageTest extends BambooClientTestCase
 {
 
+    const PREFIX = 'bambootest_';
+    const BODY = 'This will not be part of the hash key';
+    const URL = 'http://localhost/testPath';
+
+    private $_mockCache;
+
+    public function setUp() {
+        $this->_mockCache = $this->getMockBuilder('\Guzzle\Cache\CacheAdapterInterface')->getMock();
+    }
+
+    /**
+     * The body cache key is md5(url), parent's also include the body digest
+     */
     public function testBodyKey() {
-        $prefix = 'bambootest_';
-        $body = 'This will not be part of the hash key';
-        $url = 'http://localhost/testPath';
-        $expects = $prefix . md5($url);
-
-        $mockCache = $this->getMockBuilder('\Guzzle\Cache\CacheAdapterInterface')->getMock();
-        $mockCache->expects($this->at(1))
+        $expectsBody = self::PREFIX . md5(self::URL);
+        $this->_mockCache->expects($this->at(1))
             ->method('save')
-            ->with($expects);
+            ->with($expectsBody);
+        $this->_makeRequest();
+    }
 
-        $cacheStorage = new CacheStorage($mockCache, $prefix);
+    /**
+     * The header cache key is md5(method + ' ' + url)
+     */
+    public function testHeaderKey() {
+        $expectsHeader = self::PREFIX . md5('GET ' . self::URL);
+        $this->_mockCache->expects($this->at(2))
+            ->method('save')
+            ->with($expectsHeader);
+        $this->_makeRequest();
+    }
+
+    private function _makeRequest() {
+        $cacheStorage = new CacheStorage($this->_mockCache, self::PREFIX);
         $cacheStorage->cache(
-            new \Guzzle\Http\Message\Request('GET', $url),
-            new \Guzzle\Http\Message\Response(200, array(), $body)
+            new \Guzzle\Http\Message\Request('GET', self::URL),
+            new \Guzzle\Http\Message\Response(200, array(), self::BODY)
         );
     }
 
 }
+
